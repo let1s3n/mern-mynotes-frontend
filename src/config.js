851798565a97cf399927467d2a5968635e769642
let1s3n <letisendev@gmail.com>
2021-04-16 12:33:49 -0500

    asd

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
 
 if (process.env.NODE_ENV === 'production') {
 
-  const API_URL = process.env.API_URL;
+  export const API_URL = process.env.API_URL;
 } else {
-  const API_URL = process.env.REACT_APP_API_URL;
+  export const API_URL = process.env.REACT_APP_API_URL;
 }
-
-export default API_URL;
